21b8319598e795d06f08351df175a7b434f33039
mamad3212 <94684549+mamad3212@users.noreply.github.com>
2024-12-26 01:34:50 +0330

    add .gitignore file. fix #3

--- server.js
+++ server.js
@@ -4,13 +4,31 @@ const fs = require('fs');
 // create a server object:
 http.createServer(function (req, res) {
 
-  // read file 
-  fs.readFile('./index.html', 'utf8', function (err, data) {
+  if (req.url == '/') {
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end(); //end the response
-  });
-  }).listen(2222); //the server object listens on port 2222
-  
-  console.log ("Server running on port 2222");
+    fs.readFile('./index.html', 'utf8', function (err, data) {
+
+      const title = "گیتار";  
+      const content = "<h1>توضیحات در مورد گیتار</h1>";
+
+      const replace = data.replace('__title__', title).replace('__content__', content); // replace title and content in index.html
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(replace);
+      res.end();
+    });
+
+  } else if (req.url == '/style.css') {
+
+    fs.readFile('./style.css', 'utf8', function (err, data) {
+
+      if (err) return res.writeHead(500).end("Error reading style.css");
+
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.write(data);
+      res.end();
+    });
+
+}}).listen(2222); //the server object listens on port 2222
+
+console.log("Server running on port 2222");
