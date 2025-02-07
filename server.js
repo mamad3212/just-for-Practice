f04ed919f357f81d4d6a63b24a39f6bd2059858e
mamad3212 <erfanimohammad61@gmail.com>
2025-02-07 17:50:13 +0330

    connect to mysql. fix #5

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var http = require('http');
 const fs = require('fs');
+const mysql = require('mysql');
+
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'practice'
+});
+
+db.connect(function(err) {
+  console.log('connect to mysql');
+});
+
 
 // create a server object:
 http.createServer(function (req, res) {
@@ -8,28 +22,27 @@ http.createServer(function (req, res) {
 
     fs.readFile('./index.html', 'utf8', function (err, data) {
 
-      const title = "گیتار";  
+      const title = "گیتار";
       const content = "<h1>توضیحات در مورد گیتار</h1>";
 
-      const replace = data.replace('__title__', title).replace('__content__', content); // replace title and content in index.html
+      const replace = data.replace('__title__', title).replace('__content__', content);
 
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(replace);
       res.end();
     });
 
-  } else if (req.url == '/style.css') {
+  } else if (req.url == '/css/style.css') { 
 
-    fs.readFile('./style.css', 'utf8', function (err, data) {
-
-      if (err) return res.writeHead(500).end("Error reading style.css");
+    fs.readFile('./css/style.css', 'utf8', function (err, data) {
 
       res.writeHead(200, { 'Content-Type': 'text/css' });
       res.write(data);
       res.end();
     });
 
-}}).listen(2222); //the server object listens on port 2222
+  }
+
+}).listen(2222); //the server object listens on port 2222
 
 console.log("Server running on port 2222");
-//test for connect to github
